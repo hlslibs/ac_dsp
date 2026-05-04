8a49e72cd6218aa1acd7eddd5b073eb7a0706ea2
David Burnette <hlslibs@mentor.com>
2026-05-04 14:14:27 -0700

    Changes in 29.1.0 (see pdfdocs/ac_dsp_relnotes.pdf for details)

--- include/ac_dsp/ac_cic_dec_full.h
+++ include/ac_dsp/ac_cic_dec_full.h
@@ -2,11 +2,11 @@
  *                                                                        *
  *  Algorithmic C (tm) DSP Library                                        *
  *                                                                        *
- *  Software Version: 2025.4                                              *
+ *  Software Version: 2026.1                                              *
  *                                                                        *
- *  Release Date    : Thu Dec 11 10:28:09 PST 2025                        *
+ *  Release Date    : Tue Feb 10 18:33:18 PST 2026                        *
  *  Release Type    : Production Release                                  *
- *  Release Build   : 2025.4.1                                            *
+ *  Release Build   : 2026.1.0                                            *
  *                                                                        *
  *  Copyright 2018 Siemens                                                *
  *                                                                        *
